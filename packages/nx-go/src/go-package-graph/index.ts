d6cfc6bc9493a0e64528d6f94f56eb9eab16fe66
Phillip Couto <phillip@couto.in>
2022-02-04 00:00:32 -0500

    Implemented plugin to build dependency graph for go projects

---
+++ packages/nx-go/src/go-package-graph/index.ts
@@ -0,0 +1,58 @@
+import { ProjectGraph, ProjectGraphBuilder, ProjectGraphProcessorContext } from '@nrwl/devkit'
+import { extname } from 'path'
+import { execSync } from 'child_process'
+
+export const processProjectGraph = (graph: ProjectGraph, context: ProjectGraphProcessorContext) => {
+  const projectRootLookupMap: Map<string, string> = new Map()
+  for (const projectName in graph.nodes) {
+    projectRootLookupMap.set(graph.nodes[projectName].data.root, projectName)
+  }
+
+  const builder = new ProjectGraphBuilder(graph)
+  // Define dependencies using the context of files that were changed to minimize work
+  // between each run.
+  for (const projectName in context.filesToProcess) {
+    context.filesToProcess[projectName]
+      .filter((f) => extname(f.file) === '.go')
+      .map(({ file }) => ({ projectName, file, dependencies: getGoDependencies(projectRootLookupMap, file) }))
+      .filter((data) => data.dependencies && data.dependencies.length > 0)
+      .forEach(({ projectName, file, dependencies }) => {
+        for (const dependency of dependencies) {
+          builder.addExplicitDependency(projectName, file, dependency)
+        }
+      })
+  }
+
+  return builder.getUpdatedProjectGraph()
+}
+
+/**
+ * getGoDependencies will use `go list` to get dependency information from a go file
+ * @param projectRootLookup
+ * @param file
+ * @returns
+ */
+const getGoDependencies = (projectRootLookup: Map<string, string>, file: string) => {
+  const goPackageDataJson = execSync('go list -json ./' + file, { encoding: 'utf-8' })
+  const goPackage: GoPackage = JSON.parse(goPackageDataJson)
+
+  // Collect both package dependencies and test dependencies for accurate dependency
+  // tracking.
+  const listOfImports = [...(goPackage.Deps ?? []), ...(goPackage.TestImports ?? [])]
+
+  return listOfImports
+    .filter((d) => d.startsWith(goPackage.Module.Path))
+    .map((d) => d.substring(goPackage.Module.Path.length + 1))
+    .map((rootDir) => projectRootLookup.get(rootDir))
+}
+
+/**
+ * GoPackage shape needed by the code
+ */
+interface GoPackage {
+  Deps?: string[]
+  Module: {
+    Path: string
+  }
+  TestImports?: string[]
+}
---
+++ packages/nx-go/src/index.ts
@@ -0,0 +1,1 @@
+export { processProjectGraph } from './go-package-graph'
