9186e8fc9a0cd966e7057a241541a6cd201bbcb2
Phillip Couto <phillip@couto.in>
2022-02-16 06:56:52 -0500

    Fixed command failed when running in graph daemon

--- packages/nx-go/src/go-package-graph/index.ts
+++ packages/nx-go/src/go-package-graph/index.ts
@@ -1,8 +1,11 @@
 import { ProjectGraph, ProjectGraphBuilder, ProjectGraphProcessorContext } from '@nrwl/devkit'
 import { basename, extname } from 'path'
 import { execSync } from 'child_process'
+import { findNxWorkspaceRootPath } from '../utils/find-workspace-root-path'
 
 export const processProjectGraph = (graph: ProjectGraph, context: ProjectGraphProcessorContext) => {
+  const workspaceRootPath = findNxWorkspaceRootPath()
+
   const projectRootLookupMap: Map<string, string> = new Map()
   for (const projectName in graph.nodes) {
     projectRootLookupMap.set(graph.nodes[projectName].data.root, projectName)
@@ -14,7 +17,11 @@ export const processProjectGraph = (graph: ProjectGraph, context: ProjectGraphPr
   for (const projectName in context.filesToProcess) {
     context.filesToProcess[projectName]
       .filter((f) => extname(f.file) === '.go')
-      .map(({ file }) => ({ projectName, file, dependencies: getGoDependencies(projectRootLookupMap, file) }))
+      .map(({ file }) => ({
+        projectName,
+        file,
+        dependencies: getGoDependencies(workspaceRootPath, projectRootLookupMap, file),
+      }))
       .filter((data) => data.dependencies && data.dependencies.length > 0)
       .forEach(({ projectName, file, dependencies }) => {
         for (const dependency of dependencies) {
@@ -32,8 +39,8 @@ export const processProjectGraph = (graph: ProjectGraph, context: ProjectGraphPr
  * @param file
  * @returns
  */
-const getGoDependencies = (projectRootLookup: Map<string, string>, file: string) => {
-  const goPackageDataJson = execSync('go list -json ./' + file, { encoding: 'utf-8' })
+const getGoDependencies = (workspaceRootPath: string, projectRootLookup: Map<string, string>, file: string) => {
+  const goPackageDataJson = execSync('go list -json ./' + file, { encoding: 'utf-8', cwd: workspaceRootPath })
   const goPackage: GoPackage = JSON.parse(goPackageDataJson)
   const isTestFile = basename(file, '.go').endsWith('_test')
 
