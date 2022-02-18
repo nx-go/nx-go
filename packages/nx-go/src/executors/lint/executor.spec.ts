c2ac7e1ecc3fd545b1638542fd7077f200046f19
Phillip Couto <phillip@couto.in>
2022-02-17 22:25:53 -0500

    Fixed executor unit tests to at least pass
    We mock the runGoCommand to assert that the correct function is being called

--- packages/nx-go/src/executors/lint/executor.spec.ts
+++ packages/nx-go/src/executors/lint/executor.spec.ts
@@ -1,9 +1,22 @@
 import executor from './executor'
 import { LintExecutorSchema } from './schema'
 
+jest.mock('../../utils')
+import * as utils from '../../utils'
+
 const options: LintExecutorSchema = {}
 
 describe('Lint Executor', () => {
+  beforeEach(async () => {
+    // Mocks the runGoCommand
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ;(utils as any).runGoCommand = jest.fn().mockReturnValue({
+      success: true,
+    })
+  })
+
+  afterEach(() => jest.clearAllMocks())
+
   it('can run', async () => {
     const output = await executor(options, null)
     expect(output.success).toBe(true)
