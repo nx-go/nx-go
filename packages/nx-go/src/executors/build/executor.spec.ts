8f92d459e35d3da0f11421b6fd5db714aa411332
Phillip Couto <phillip@couto.in>
2022-02-20 17:57:13 -0500

    test: fixed executor unit tests to at least pass (#54)
    We mock the runGoCommand to assert that the correct function is being called
    
    Co-authored-by: beeman <borggreve@gmail.com>

--- packages/nx-go/src/executors/build/executor.spec.ts
+++ packages/nx-go/src/executors/build/executor.spec.ts
@@ -1,9 +1,21 @@
 import executor from './executor'
 import { BuildExecutorSchema } from './schema'
 
+jest.mock('../../utils')
+import * as utils from '../../utils'
+
 const options: BuildExecutorSchema = { main: '', outputPath: '' }
 
 describe('Build Executor', () => {
+  beforeEach(async () => {
+    // Mocks the runGoCommand
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ;(utils as any).runGoCommand = jest.fn().mockReturnValue({
+      success: true,
+    })
+  })
+
+  afterEach(() => jest.clearAllMocks())
   it('can run', async () => {
     const output = await executor(options, null)
     expect(output.success).toBe(true)
--- packages/nx-go/src/executors/test/executor.spec.ts
+++ packages/nx-go/src/executors/test/executor.spec.ts
@@ -1,9 +1,22 @@
 import { TestExecutorSchema } from './schema'
 import executor from './executor'
 
+jest.mock('../../utils')
+import * as utils from '../../utils'
+
 const options: TestExecutorSchema = {}
 
 describe('Test Executor', () => {
+  beforeEach(async () => {
+    // Mocks the runGoCommand
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ;(utils as any).runGoCommand = jest.fn().mockReturnValue({
+      success: true,
+    })
+  })
+
+  afterEach(() => jest.clearAllMocks())
+
   it('can run', async () => {
     const output = await executor(options, null)
     expect(output.success).toBe(true)
