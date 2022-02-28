141c98291e9976634809e87713e85ae8c5053ab2
Phillip Couto <phillip@couto.in>
2022-02-27 20:42:06 -0500

    feat: ensure go.mod is added to implicit dependencies (#57)
    * Updated app generator to ensure go.mod is added to implicit dependencies
    
    If go.mod does not exist then nothing happens.
    
    * Added migration to add go.mod when upgrading nx-go

--- packages/nx-go/src/utils/create-go-mod.helper.ts
+++ packages/nx-go/src/utils/create-go-mod.helper.ts
@@ -1,4 +1,5 @@
 import { Tree } from '@nrwl/devkit'
+import { ensureGoModDependency } from './ensureGoModDependency'
 import { NormalizedSchema } from './normalized-schema.interface'
 
 export function createGoMod(tree: Tree, options: NormalizedSchema) {
@@ -7,5 +8,6 @@ export function createGoMod(tree: Tree, options: NormalizedSchema) {
     if (!tree.exists(`${modFile}`)) {
       tree.write(`${modFile}`, `module ${options.npmScope}\n`)
     }
+    ensureGoModDependency(tree)
   }
 }
