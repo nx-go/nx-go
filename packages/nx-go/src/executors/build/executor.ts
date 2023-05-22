b706432fa67e26178f83952c93d402b301a61ef9
Phillip Couto <pcouto@eckler.ca>
2023-05-22 08:51:44 -0400

    feat: pass flags and environment variables to go build
    added unit tests to build executor

--- packages/nx-go/src/executors/build/executor.ts
+++ packages/nx-go/src/executors/build/executor.ts
@@ -5,6 +5,18 @@ import { BuildExecutorSchema } from './schema'
 export default async function runExecutor(options: BuildExecutorSchema, context: ExecutorContext) {
   const mainFile = `${options.main}`
   const output = `-o ${options.outputPath}${process.platform === 'win32' ? '.exe' : ''}`
+  const params: string[] = []
+  if (options.outputPath) {
+    params.push(output)
+  }
+  if (options.flags) {
+    params.push(...options.flags)
+  }
+  if (options.main) {
+    params.push(mainFile)
+  }
 
-  return runGoCommand(context, 'build', [output, mainFile])
+  const commandOptions = options.env ? { env: options.env } : undefined
+
+  return runGoCommand(context, 'build', params, commandOptions)
 }
