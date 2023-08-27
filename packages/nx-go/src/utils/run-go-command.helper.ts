939212844907897e738ff24bec43c0ecda0645a1
Phillip Couto <phillip@couto.in>
2023-08-27 07:08:41 -0400

    feat: pass flags and environment variables to go build (#89)
    added unit tests to build executor
    
    Co-authored-by: Phillip Couto <pcouto@eckler.ca>
    Co-authored-by: beeman <beeman@beeman.nl>

--- packages/nx-go/src/utils/run-go-command.helper.ts
+++ packages/nx-go/src/utils/run-go-command.helper.ts
@@ -1,12 +1,25 @@
 import { ExecutorContext } from '@nrwl/devkit'
 import { execSync } from 'child_process'
 
-export function runGoCommand(
+export interface RunGoCommandOptions {
+  cwd?: string
+  cmd?: string
+  env?: { [key: string]: string }
+}
+
+export type RunGoCommand = (
   context: ExecutorContext,
   command: 'build' | 'fmt' | 'run' | 'test',
   params: string[],
-  options: { cwd?: string; cmd?: string } = {},
-): { success: boolean } {
+  options: RunGoCommandOptions,
+) => { success: boolean }
+
+export const runGoCommand: RunGoCommand = (
+  context: ExecutorContext,
+  command: 'build' | 'fmt' | 'run' | 'test',
+  params: string[],
+  options: RunGoCommandOptions = {},
+) => {
   // Take the parameters or set defaults
   const cmd = options.cmd || 'go'
   const cwd = options.cwd || process.cwd()
@@ -14,9 +27,12 @@ export function runGoCommand(
   // Create the command to execute
   const execute = `${cmd} ${command} ${params.join(' ')}`
 
+  // Pass along the environment variables to the process
+  const env = options.env ? { ...process.env, ...options.env } : process.env
+
   try {
     console.log(`Executing command: ${execute}`)
-    execSync(execute, { cwd, stdio: [0, 1, 2] })
+    execSync(execute, { cwd, stdio: [0, 1, 2], env })
     return { success: true }
   } catch (e) {
     console.error(`Failed to execute command: ${execute}`, e)
